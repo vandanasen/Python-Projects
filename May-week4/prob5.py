b4f6bc41d83b554d5159720266add25fb5a2bc99
vandanasen <vandy_senthil@yahoo.com>
2018-05-22 17:25:16 -0400

    prob5 added

---
+++ May-week4/prob5.py
@@ -0,0 +1,22 @@
+"""
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+"""
+class myclass:
+    def __init__(self,str1):
+        self.str1 = str1
+    def __str__(self):
+        return "" \
+               "string".format(self.str1)
+    def getstring(self):
+        self.str1=input('enter a string')
+
+    def printstring(self):
+        print(self.str1)
+ob=myclass('')
+f=ob.getstring()
+g=ob.printstring()
+
+
